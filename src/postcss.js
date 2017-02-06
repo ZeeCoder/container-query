32b9117efd64ea7af070493664d4bc5c8cf10e59
Viktor Hubert <rpgmorpheus@gmail.com>
2017-02-06 21:23:20 +0000

    base values / queries

--- src/postcss.js
+++ src/postcss.js
@@ -4,6 +4,38 @@ const camelCase = require('lodash.camelCase');
 const CUNIT__HEIGHT = 'ch';
 const CUNIT__WIDTH = 'cw';
 
+function addDefaultsToContainer (container, defaultElementStyles, defaultElementValues) {
+    let defaultQuery = { elements: [] };
+    for (let elementSelector in defaultElementStyles) {
+        let defaultElement = {
+            selector: elementSelector,
+            styles: {},
+        };
+
+        defaultElementStyles[elementSelector].forEach((prop) => {
+            defaultElement.styles[prop] = '';
+        });
+
+        defaultQuery.elements.push(defaultElement);
+    }
+    container.queries.unshift(defaultQuery);
+
+    let defaultValue = { elements: [] };
+    for (let elementSelector in defaultElementValues) {
+        let defaultElement = {
+            selector: elementSelector,
+            values: {},
+        };
+
+        defaultElementValues[elementSelector].forEach((prop) => {
+            defaultElement.values[prop] = '';
+        });
+
+        defaultValue.elements.push(defaultElement);
+    }
+    container.values.unshift(defaultValue);
+}
+
 module.exports = postcss.plugin('container-query', function myplugin(options) {
 
     return function (root) {
@@ -13,16 +45,27 @@ module.exports = postcss.plugin('container-query', function myplugin(options) {
         options = options || {};
 
         // Processing code will be added her
-        let currentContainer;
+        let currentContainer = null;
+        let defaultElementStyles = {};
+        let defaultElementValues = {};
         root.walkAtRules((rule) => {
             // todo: handle issues here, when for a @container declaration there's no defined-container
+
             if (rule.name === 'define-container') {
                 containers[rule.parent.selector] = {
                     selector: rule.parent.selector,
                     queries: [],
                     values: [],
                 };
+
+                if (currentContainer !== null) {
+                    addDefaultsToContainer(containers[currentContainer], defaultElementStyles, defaultElementValues);
+                }
+
                 currentContainer = rule.parent.selector;
+                defaultElementStyles = {};
+                defaultElementValues = {};
+
             } else if (rule.name === 'container') {
                 let query = { conditions: [], elements: [] };
                 let value = { conditions: [], elements: [] };
@@ -63,6 +106,12 @@ module.exports = postcss.plugin('container-query', function myplugin(options) {
                         ) {
                             queryElement.styles[prop] = declaration.value;
                             attachQuery = true;
+
+                            if (typeof defaultElementStyles[rule.selector] === 'undefined') {
+                                defaultElementStyles[rule.selector] = [];
+                            }
+
+                            defaultElementStyles[rule.selector].push(prop);
                         } else {
 
                             let valueArr = declaration.value.match(/(\d*)([a-z]*)/i);
@@ -71,6 +120,12 @@ module.exports = postcss.plugin('container-query', function myplugin(options) {
 
                             valueElement.values[prop] = valueArr;
                             attachValue = true;
+
+                            if (typeof defaultElementValues[rule.selector] === 'undefined') {
+                                defaultElementValues[rule.selector] = [];
+                            }
+
+                            defaultElementValues[rule.selector].push(prop);
                         }
                     });
 
@@ -90,6 +145,8 @@ module.exports = postcss.plugin('container-query', function myplugin(options) {
                 }
             }
         });
+
+        addDefaultsToContainer(containers[currentContainer], defaultElementStyles, defaultElementValues);
 
         console.log(JSON.stringify(containers, null, 2));
     };
