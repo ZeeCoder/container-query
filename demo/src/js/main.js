b42e413dd9345a0b8db1c7fe05b40f2938096e4e
Viktor Hubert <rpgmorpheus@gmail.com>
2017-03-02 22:52:34 +0000

    Demo improvements

--- demo/src/js/main.js
+++ demo/src/js/main.js
@@ -10,11 +10,15 @@ function initialiseContainer (jsonData) {
     for (let i = 0; i < htmlElementsLength; i++) {
         const containerInstance = new Container(htmlElements[i], jsonData);
         window.addEventListener('resize', containerInstance.adjust);
+        requestAnimationFrame(() => {
+            containerInstance.adjust();
+        });
     }
 }
 
 initialiseContainer(require('../css/components/user/user.json'));
 initialiseContainer(require('../css/components/social-link/social-link.json'));
+initialiseContainer(require('../css/components/social-container/social-container.json'));
 
 
 // import initialiseAllContainers from '../initialiseAllContainers';
