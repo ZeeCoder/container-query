693e260b1f82a6adc2861522f8c4afa01771b71a
Viktor Hubert <rpgmorpheus@gmail.com>
2017-05-29 23:04:03 +0100

    README update

--- demo/gulpfile.js
+++ demo/gulpfile.js
@@ -1,49 +1,60 @@
-'use strict';
+"use strict";
 
-const gulp = require('gulp');
-const fs = require('fs');
-const path = require('path');
-const postcss = require('gulp-postcss');
-const rename = require('gulp-rename');
-const nested = require('postcss-nested');
-const autoprefixer = require('autoprefixer');
-const postcssImport = require('postcss-import');
-const containerQuery = require('@zeecoder/container-query/containerQuery');
-const writeFileSync = require('fs').writeFileSync;
+const gulp = require("gulp");
+const fs = require("fs");
+const path = require("path");
+const postcss = require("gulp-postcss");
+const rename = require("gulp-rename");
+const nested = require("postcss-nested");
+const autoprefixer = require("autoprefixer");
+const postcssImport = require("postcss-import");
+const containerQuery = require("@zeecoder/container-query/containerQuery");
+const writeFileSync = require("fs").writeFileSync;
 
-function containerSelectorToFilename (selector) {
+function containerSelectorToFilename(selector) {
     return selector.substr(1);
 }
 
-gulp.task('css', function () {
-    return gulp.src('src/css/main.pcss')
-        .pipe(postcss([
-            postcssImport(),
-            nested(),
-            autoprefixer(),
-            containerQuery({
-                getJSON: (cssPath, containers) => {
-                    // Saving the container query stats individually
-                    for (let containerSelector in containers) {
-                        let component = containerSelectorToFilename(containerSelector);
+gulp.task("css", function() {
+    return gulp
+        .src("src/css/main.pcss")
+        .pipe(
+            postcss([
+                postcssImport(),
+                nested({
+                    bubble: ["container"]
+                }),
+                autoprefixer(),
+                containerQuery({
+                    getJSON: (cssPath, containers) => {
+                        // Saving the container query stats individually
+                        for (let containerSelector in containers) {
+                            let component = containerSelectorToFilename(
+                                containerSelector
+                            );
+                            writeFileSync(
+                                `${__dirname}/src/css/components/${component}/${component}.json`,
+                                JSON.stringify(containers[containerSelector])
+                            );
+                        }
+
+                        // Then saving the container names
                         writeFileSync(
-                            `${__dirname}/src/css/components/${component}/${component}.json`,
-                            JSON.stringify(containers[containerSelector])
+                            `${__dirname}/src/js/containers.json`,
+                            JSON.stringify(
+                                Object.keys(containers).map(
+                                    containerSelectorToFilename
+                                )
+                            )
                         );
                     }
-
-                    // Then saving the container names
-                    writeFileSync(
-                        `${__dirname}/src/js/containers.json`,
-                        JSON.stringify(Object.keys(containers).map(containerSelectorToFilename))
-                    );
-                }
-            }),
-        ]))
-        .pipe( rename('main.css') )
-        .pipe( gulp.dest('web/dist') );
+                })
+            ])
+        )
+        .pipe(rename("main.css"))
+        .pipe(gulp.dest("web/dist"));
 });
 
-gulp.task('watch', function() {
-    gulp.watch('src/**/*.pcss', ['css']);
+gulp.task("watch", function() {
+    gulp.watch("src/**/*.pcss", ["css"]);
 });
