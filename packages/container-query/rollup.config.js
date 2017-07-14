02c391176e2a6f1530e6e0b258cbe61836d0a141
Viktor Hubert <rpgmorpheus@gmail.com>
2017-07-14 21:20:03 +0100

    wip on making the bundles smaller in size

--- packages/container-query/rollup.config.js
+++ packages/container-query/rollup.config.js
@@ -1,18 +1,31 @@
 import commonjs from "rollup-plugin-commonjs";
 import nodeResolve from "rollup-plugin-node-resolve";
 import flow from "rollup-plugin-flow";
+import uglify from "rollup-plugin-uglify";
+import babel from "rollup-plugin-babel";
 
 export default {
     entry: "src/Container.js",
     targets: [
         { dest: "dist/bundle.cjs.js", format: "cjs" },
-        { dest: "dist/bundle.es.js", format: "es" },
+        // { dest: "dist/bundle.es.js", format: "es" },
         {
             dest: "dist/bundle.umd.js",
-
             format: "umd",
             moduleName: "ContainerQuery"
         }
     ],
-    plugins: [flow(), commonjs(), nodeResolve()]
+    plugins: [
+        flow(),
+        commonjs(),
+        nodeResolve(),
+        babel({
+            exclude: "node_modules/**",
+            babelrc: false,
+            runtimeHelpers: true,
+            presets: [["es2015", { modules: false }]],
+            plugins: []
+        }),
+        uglify()
+    ]
 };
