c8741d88cda897e018c454d50b822e3e0fcbe25d
Viktor Hubert <rpgmorpheus@gmail.com>
2017-07-17 09:14:12 +0100

    wip on processConfig

--- packages/container-query/src/processConfig.js
+++ packages/container-query/src/processConfig.js
@@ -1,23 +1,21 @@
+// @flow
 import objectAssign from "object-assign";
 import getConditionFunction from "./getConditionFunction";
+import type { QueryStats } from "../../common/src/types";
 
 /**
  * Returns an processed copy of the given configuration object.
  * Enhancements:
  * - Condition arrays are converted to functions that accept container dimensions
- *
- * @param {Object} origConfig
- *
- * @returns {Object|null} Return null for invalid configurations
  */
-export default function processConfig(origConfig) {
+export default function processConfig(origConfig: QueryStats): QueryStats {
   // Validate configuration before processing
   if (
     typeof origConfig !== "object" ||
     typeof origConfig.selector !== "string" ||
     !Array.isArray(origConfig.queries)
   ) {
-    return null;
+    throw new Error("better message needed");
   }
 
   // Configuration seems valid, process it
