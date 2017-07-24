d041cad777a325d0aef7405bcffc46b8b9b43235
Viktor Hubert <rpgmorpheus@gmail.com>
2017-07-24 23:29:12 +0100

    Using babel-preset-env for the packages

--- packages/container-query/rollup.config.js
+++ packages/container-query/rollup.config.js
@@ -2,24 +2,26 @@ import commonjs from "rollup-plugin-commonjs";
 import flow from "rollup-plugin-flow";
 import babel from "rollup-plugin-babel";
 import uglify from "rollup-plugin-uglify";
+import fs from "fs";
+
+const babelrc = JSON.parse(fs.readFileSync("./.babelrc").toString());
+
+// To make jest work, we have a "base" babelrc config in the directory, which
+// however needs some tweaks to make rollup happy too.
+babelrc.babelrc = false;
+babelrc.plugins.push("external-helpers");
+babelrc.presets.forEach(preset => {
+  if (!Array.isArray(preset) || preset[0] !== "env") {
+    return;
+  }
+
+  preset[1].modules = false;
+});
 
 export default {
   entry: "src/Container.js",
   targets: [{ dest: "dist/bundle.cjs.js", format: "cjs" }],
-  plugins: [
-    flow(),
-    commonjs(),
-    babel({
-      babelrc: false,
-      // For some reason if I add this to .babelrc, it doesn't
-      // include the babel helpers in the bundle.
-      plugins: ["external-helpers"],
-      // The following also didn't work in a .babelrc, since that messed with
-      // jest's .babelrc, resulting in errors on import statements...
-      presets: [["es2015", { modules: false }]]
-    }),
-    uglify()
-  ],
+  plugins: [flow(), commonjs(), babel(babelrc), uglify()],
   external: [
     "es6-weak-map",
     "object-assign",
