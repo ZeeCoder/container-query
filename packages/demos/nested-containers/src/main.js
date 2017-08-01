f30094dfd0355db9f6a7eff30e133fd6c02d04fe
Viktor Hubert <rpgmorpheus@gmail.com>
2017-08-01 21:57:39 +0100

    Object.assign polyfill for the demos

--- packages/demos/nested-containers/src/main.js
+++ packages/demos/nested-containers/src/main.js
@@ -1,3 +1,6 @@
+// polyfilling
+import "core-js/fn/object/assign";
+
 import ReactDOM from "react-dom";
 import React from "react";
 import App from "./components/App/App";
--- packages/demos/react-social/src/main.js
+++ packages/demos/react-social/src/main.js
@@ -1,3 +1,6 @@
+// polyfilling
+import "core-js/fn/object/assign";
+
 import App from "./components/App/App";
 import React from "react";
 import ReactDOM from "react-dom";
