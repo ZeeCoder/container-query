531a632993b30d1c806e89bd8ef3497bc61d349f
Viktor Hubert <rpgmorpheus@gmail.com>
2018-01-27 21:06:57 +0000

    build refactors

--- packages/container-query/rollup/rollup.shared.js
+++ packages/container-query/rollup/rollup.shared.js
@@ -1,26 +1,9 @@
-import fs from "fs";
 import flow from "rollup-plugin-flow";
 import babel from "rollup-plugin-babel";
 
-const babelrc = JSON.parse(
-  fs.readFileSync(__dirname + "/../.babelrc").toString()
-);
-
-// To make jest work, we have a "base" babelrc config in the directory, which
-// however needs some tweaks to make rollup happy too.
-babelrc.babelrc = false;
-babelrc.plugins.push("external-helpers");
-babelrc.presets.forEach(preset => {
-  if (!Array.isArray(preset) || preset[0] !== "env") {
-    return;
-  }
-
-  preset[1].modules = false;
-});
-
 export default {
   input: __dirname + "/../src/Container.js",
-  plugins: [flow(), babel(babelrc)],
+  plugins: [flow(), babel()],
   external: [
     "es6-weak-map",
     "object-assign",
--- packages/postcss-container-query/rollup.config.js
+++ packages/postcss-container-query/rollup.config.js
@@ -3,23 +3,9 @@ import babel from "rollup-plugin-babel";
 import babili from "rollup-plugin-babili";
 import fs from "fs";
 
-const babelrc = JSON.parse(fs.readFileSync("./.babelrc").toString());
-
-// To make jest work, we have a "base" babelrc config in the directory, which
-// however needs some tweaks to make rollup happy too.
-babelrc.babelrc = false;
-babelrc.plugins.push("external-helpers");
-babelrc.presets.forEach(preset => {
-  if (!Array.isArray(preset) || preset[0] !== "env") {
-    return;
-  }
-
-  preset[1].modules = false;
-});
-
 export default {
   input: "src/containerQuery.js",
   output: [{ file: "dist/bundle.cjs.js", format: "cjs" }],
-  plugins: [flow(), babel(babelrc), babili()],
-  external: ["postcss", "lodash/trim", "lodash/camelCase", "fs"]
+  plugins: [flow(), babel(), babili()],
+  external: ["postcss", "lodash/trim", "fs"]
 };
