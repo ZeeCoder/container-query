1b0c80d7719f94d4c2f7c933806b5bb2f07344ef
Viktor Hubert <rpgmorpheus@gmail.com>
2018-02-25 00:58:03 +0000

    react-container-query dep upgrades

--- packages/react-container-query/rollup/rollup.shared.js
+++ packages/react-container-query/rollup/rollup.shared.js
@@ -1,8 +1,9 @@
 import babel from "rollup-plugin-babel";
+import uglify from "rollup-plugin-uglify";
 
 export default {
   input: __dirname + "/../src/index.js",
-  plugins: [babel()],
+  plugins: [babel(), uglify()],
   external: [
     "react",
     "react-dom",
