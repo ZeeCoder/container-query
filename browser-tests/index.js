911913b9801634456c69de8139f6e1fbae5e29d7
Viktor Hubert <rpgmorpheus@gmail.com>
2018-12-14 00:43:00 +0000

    Adding karma

---
+++ browser-tests/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Test = () => <div id="asd" style={{ background: "grey" }} />;
+
+// todo figure out how to make async-await work in parcel
+// todo make sure parcel transpiles down to IE10
+it("should render", () => {
+  const div = document.createElement("div");
+  document.body.appendChild(div);
+
+  ReactDOM.render(<Test />, div);
+
+  const rendered = document.querySelector("#asd");
+
+  expect(rendered.style.backgroundColor).toBe("grey");
+});
