384e74cccd945aaba746c4175ca8410fca11e23c
Viktor Hubert <rpgmorpheus@gmail.com>
2019-01-17 00:28:28 +0000

    first non oocss tests

---
+++ tests/react/non-oocss/index.js
@@ -0,0 +1,257 @@
+import React from "react";
+import Container from "./Container/Container";
+import {
+  renderTestComponent,
+  changeRootSize,
+  clearDOM,
+  waitForElementToHaveStyle
+} from "../../utils";
+import { getByTestId } from "dom-testing-library";
+
+// Features covered:
+// - Non OOCSS classes should work (for example, if it's not BEM)
+// - If an "element" class is used somewhere, but has no container, then it shouldn't change styles
+// - Containers should have boundaries: a container that has another container
+//   as a child, should not affect the latter even if they're the same container types
+// - Tests whether the lib can work with CSS-modules built in to css-loader
+// - Tests r-units
+// todo make the tests assert whole numbers, to make cross-browser testing easier
+// todo add css-modules, and export the classes for reuse
+describe("Non OOCSS", () => {
+  const refs = {};
+  beforeAll(() => {
+    clearDOM();
+    renderTestComponent(
+      <Container id="1">
+        <Container id="2">
+          <Container id="3" />
+        </Container>
+      </Container>,
+      {
+        width: 100,
+        height: 100
+      }
+    );
+
+    refs.root1 = getByTestId(document.body, "root-1");
+    refs.marker1 = getByTestId(refs.root1, "marker-1");
+    refs.child1 = getByTestId(refs.root1, "child-1");
+    refs.root2 = getByTestId(document.body, "root-2");
+    refs.marker2 = getByTestId(refs.root2, "marker-2");
+    refs.child2 = getByTestId(refs.root2, "child-2");
+    refs.root3 = getByTestId(document.body, "root-3");
+    refs.marker3 = getByTestId(refs.root3, "marker-3");
+
+    const wrapper = document.createElement("div");
+    wrapper.style.width = "100px";
+    wrapper.style.height = "100px";
+    wrapper.style.position = "absolute";
+    wrapper.style.left = "0";
+    wrapper.style.bottom = "0";
+
+    refs.unrelatedContainer = document.createElement("div");
+    refs.unrelatedContainer.className = "container";
+    wrapper.appendChild(refs.unrelatedContainer);
+    refs.unrelatedMarker = document.createElement("div");
+    refs.unrelatedMarker.className = "marker";
+    wrapper.appendChild(refs.unrelatedMarker);
+    refs.unrelatedChild = document.createElement("div");
+    refs.unrelatedChild.className = "child";
+    wrapper.appendChild(refs.unrelatedChild);
+
+    document.body.appendChild(wrapper);
+  });
+
+  /**
+   * Even though these elements have the classes used in the tested containers,
+   * they should at no point change styles, as they are not inside a container.
+   * @return {Promise}
+   */
+  const expectUnrelatedElementsToBeUntouched = async () => {
+    await waitForElementToHaveStyle(refs.unrelatedContainer, {
+      width: "100px",
+      height: "100px"
+    });
+    await waitForElementToHaveStyle(refs.unrelatedMarker, {
+      width: "0px",
+      height: "100px",
+      backgroundColor: "rgb(255, 0, 0)"
+    });
+    await waitForElementToHaveStyle(refs.unrelatedChild, {
+      width: "0px",
+      height: "0px"
+    });
+  };
+
+  it("should render container query styles with non-oocss classnames", async () => {
+    await waitForElementToHaveStyle(refs.marker1, {
+      backgroundColor: "rgb(255, 0, 0)",
+      width: "30px"
+    });
+
+    await waitForElementToHaveStyle(refs.marker2, {
+      backgroundColor: "rgb(255, 0, 0)",
+      width: "21px"
+    });
+
+    await waitForElementToHaveStyle(refs.marker3, {
+      backgroundColor: "rgb(255, 0, 0)",
+      width: "14.6875px"
+    });
+
+    // Child wrappers should be adjusted with rh
+    await waitForElementToHaveStyle(refs.child1, {
+      height: "90px"
+    });
+    await waitForElementToHaveStyle(refs.child2, {
+      height: "81px"
+    });
+
+    await expectUnrelatedElementsToBeUntouched();
+  });
+
+  it("should change the first container's styles only", async () => {
+    changeRootSize({ width: 101 });
+
+    await waitForElementToHaveStyle(refs.marker1, {
+      backgroundColor: "rgb(0, 255, 0)",
+      width: "30.2969px"
+    });
+
+    await waitForElementToHaveStyle(refs.marker2, {
+      backgroundColor: "rgb(255, 0, 0)",
+      width: "21.2031px"
+    });
+
+    await waitForElementToHaveStyle(refs.marker3, {
+      backgroundColor: "rgb(255, 0, 0)",
+      width: "14.8281px"
+    });
+
+    // Child wrapper heights should not change
+    await waitForElementToHaveStyle(refs.child1, {
+      height: "90px"
+    });
+    await waitForElementToHaveStyle(refs.child2, {
+      height: "81px"
+    });
+
+    await expectUnrelatedElementsToBeUntouched();
+  });
+
+  it("should change the second container's styles only", async () => {
+    changeRootSize({ width: 143 });
+
+    await waitForElementToHaveStyle(refs.marker1, {
+      backgroundColor: "rgb(0, 0, 255)",
+      width: "42.8906px"
+    });
+
+    await waitForElementToHaveStyle(refs.marker2, {
+      backgroundColor: "rgb(0, 255, 0)",
+      width: "30.0156px"
+    });
+
+    await waitForElementToHaveStyle(refs.marker3, {
+      backgroundColor: "rgb(255, 0, 0)",
+      width: "21.0156px"
+    });
+
+    // Child wrapper heights should not change
+    await waitForElementToHaveStyle(refs.child1, {
+      height: "90px"
+    });
+    await waitForElementToHaveStyle(refs.child2, {
+      height: "81px"
+    });
+
+    await expectUnrelatedElementsToBeUntouched();
+  });
+
+  it("should change the third container's styles only", async () => {
+    changeRootSize({ width: 205 });
+
+    await waitForElementToHaveStyle(refs.marker1, {
+      backgroundColor: "rgb(0, 0, 255)",
+      width: "61.5px"
+    });
+
+    await waitForElementToHaveStyle(refs.marker2, {
+      backgroundColor: "rgb(0, 0, 255)",
+      width: "43.0469px"
+    });
+
+    await waitForElementToHaveStyle(refs.marker3, {
+      backgroundColor: "rgb(0, 255, 0)",
+      width: "30.125px"
+    });
+
+    // Child wrapper heights should not change
+    await waitForElementToHaveStyle(refs.child1, {
+      height: "90px"
+    });
+    await waitForElementToHaveStyle(refs.child2, {
+      height: "81px"
+    });
+
+    await expectUnrelatedElementsToBeUntouched();
+  });
+
+  it("should change the last container once again", async () => {
+    changeRootSize({ width: 286 });
+
+    await waitForElementToHaveStyle(refs.marker1, {
+      backgroundColor: "rgb(0, 0, 255)",
+      width: "85.7969px"
+    });
+
+    await waitForElementToHaveStyle(refs.marker2, {
+      backgroundColor: "rgb(0, 0, 255)",
+      width: "60.0469px"
+    });
+
+    await waitForElementToHaveStyle(refs.marker3, {
+      backgroundColor: "rgb(0, 0, 255)",
+      width: "42.0312px"
+    });
+
+    // Child wrapper heights should not change
+    await waitForElementToHaveStyle(refs.child1, {
+      height: "90px"
+    });
+    await waitForElementToHaveStyle(refs.child2, {
+      height: "81px"
+    });
+
+    await expectUnrelatedElementsToBeUntouched();
+  });
+
+  it("should be able reset to the default styles, with a different height", async () => {
+    changeRootSize({ width: 100, height: 200 });
+
+    await waitForElementToHaveStyle(refs.marker1, {
+      backgroundColor: "rgb(255, 0, 0)",
+      width: "30px"
+    });
+
+    await waitForElementToHaveStyle(refs.marker2, {
+      backgroundColor: "rgb(255, 0, 0)",
+      width: "21px"
+    });
+
+    await waitForElementToHaveStyle(refs.marker3, {
+      backgroundColor: "rgb(255, 0, 0)",
+      width: "14.6875px"
+    });
+
+    // Child wrappers should be adjusted with rh
+    await waitForElementToHaveStyle(refs.child1, {
+      height: "180px"
+    });
+    await waitForElementToHaveStyle(refs.child2, {
+      height: "162px"
+    });
+
+    await expectUnrelatedElementsToBeUntouched();
+  });
+});
